8c47783d056e45795f42d9ebb2e87e402d59770c
hismatullin_er <eldarhis04@gmail.com>
2024-02-26 15:20:43 +0400

    7.6.4 edited 7.7.5 add

--- 7.7.4unit.py
+++ 7.7.4unit.py
@@ -1,10 +1,27 @@
-mx = 0
-s = 0
-for i in range(11):
+# mx = 0
+# s = 0
+# for i in range(10):
+#     x = int(input())
+#     if x < 0:
+#         s = x
+#     if x > mx:
+#         mx = x
+# print(s)
+# print(mx)
+
+max = 0
+sum = 0
+
+for i in range(10):
     x = int(input())
     if x < 0:
-        s = x
-    if x > mx:
-        mx = x
-print(s)
-print(mx)
+        sum = sum + x
+        if x > max:
+            max = x
+        else:
+            print ("NO")
+if sum==max:
+    print ("NO")
+else:
+    print (sum)
+    print (max)
