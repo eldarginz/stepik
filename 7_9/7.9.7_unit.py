db77ba335d4bbd1662f0ec16bb10c266f479aee1
hismatullin_er <eldarhis04@gmail.com>
2024-02-27 14:14:24 +0400

    7.9.7 done

--- 7_9/7.9.7_unit.py
+++ 7_9/7.9.7_unit.py
@@ -1,20 +1,10 @@
-def prime_num(x):
-    if x == 1:
-        return False
-    else:
+a = int(input())
+b = int(input())
 
-
-
-# a = int(input())
-# b = int(input())
-#
-# for i in range(a, b + 1):
-#     if prime_num(i) == True:
-#
-
-if prime_num(7):
-    print('True')
-else:
-    print('False')
-
-# test vscode
+for x in range (a, b+1):
+    count = 0
+    for i in range (1, x+1):
+        if x % i == 0:
+            count += 1
+    if count == 2:
+        print(x)
