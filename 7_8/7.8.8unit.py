ff459edef29dc82f1633e2abf7e05a68b33bd265
hismatullin_er <eldarhis04@gmail.com>
2024-02-27 09:21:06 +0400

    done 7.8.8 add 7.8.9

--- 7_8/7.8.8unit.py
+++ 7_8/7.8.8unit.py
@@ -1,18 +1,19 @@
 n = int(input())
 count = n//2
 
-
-
+# часть 1
 for i in range(count):
     for j in range(i+1):
         print('*', end='')
     print('')
 
+# центр
 for j in range(count+1):
     print('*', end='')
 print('')
 
-for i in range(-count, 1):
-    for j in range(i+1):
+# часть 2
+for i in range(count, 0, -1):
+    for j in range(i):
         print('*', end='')
     print('')
