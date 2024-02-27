9aff63bd9a413e7f8e6c45c44e2cf6c578eeb987
hismatullin_er <eldarhis04@gmail.com>
2024-02-27 13:40:20 +0400

    7.9.7 add

---
+++ 7_9/7.9.7_unit.py
@@ -0,0 +1,18 @@
+def prime_num(x):
+    if x == 1:
+        return False
+    else:
+
+
+
+# a = int(input())
+# b = int(input())
+#
+# for i in range(a, b + 1):
+#     if prime_num(i) == True:
+#
+
+if prime_num(7) == True:
+    print('True')
+else:
+    print('False')
