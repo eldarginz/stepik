5c47db2561c82947f37c77af5dc92d69f4616136
hismatullin_er <eldarhis04@gmail.com>
2024-03-04 14:06:47 +0400

    11.2 done

---
+++ 11_2/11.2.7_unit.py
@@ -0,0 +1,5 @@
+rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+rainbow[3] = 'Зеленый'
+rainbow[-1] = 'Фиолетовый'
+
+print(rainbow)
---
+++ 11_2/11.2.8_unit.py
@@ -0,0 +1,2 @@
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+print(languages[-1::-1])
