934d75c90e8cdf2a1d870f876b7ba9d8e0ea751c
hismatullin_er <eldarhis04@gmail.com>
2024-03-04 09:42:32 +0400

    9.5 done

---
+++ 9_5/9.5.7_unit.py
@@ -0,0 +1,6 @@
+year = '2010'
+price = '10k'
+value = 'Bitcoin'
+s = 'In {0}, someone paid {1} {2} for two pizzas.'.format(year, price, value)
+
+print(s)
---
+++ 9_5/9.5.8_unit.py
@@ -0,0 +1,5 @@
+year = 2010
+amount = '10K'
+currency = 'Bitcoin'
+
+print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
