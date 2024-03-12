69f172386bad03bb675249ebdb4b173c994a5dec
hismatullin_er <eldarhis04@gmail.com>
2024-03-12 13:36:11 +0400

    new file:   13.5/13.5.10_unit.py 	new file:   13.5/13.5.2_unit.py 	new file:   13.5/13.5.3_unit.py 	new file:   13.5/13.5.4_unit.py 	new file:   13.5/13.5.5_unit.py 	new file:   13.5/13.5.6_unit.py 	new file:   13.5/13.5.7_unit.py 	new file:   13.5/13.5.8_unit.py 	new file:   13.5/13.5.9_unit.py

---
+++ 13.5/13.5.3_unit.py
@@ -0,0 +1,12 @@
+# объявление функции
+def is_prime(num):
+    d = 2
+    while n % d != 0:
+        d += 1
+    return d == n
+
+# считываем данные
+n = int(input())
+
+# вызываем функцию
+print(is_prime(n))
