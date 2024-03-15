5d28994bf5a00aad11ef422adc9f5d57fe5056eb
hismatullin_er <eldarhis04@gmail.com>
2024-03-15 14:08:18 +0400

    game update

--- 15.2 Угадайка чисел/game.py
+++ 15.2 Угадайка чисел/game.py
@@ -5,15 +5,29 @@
 
 import random
 num = random.randint(1,100)
-print(num)
+
+def is_valid(num):
+    if int(num) < 0 or int(num) > 100:
+        return False
+    else:
+        return True
+
+#print(num)
+counter = 1
 
 print ('Попробуй угадать загаданное число:')
 while True:
     user_num = int(input('Введите число от 1 до 100: '))
-    if user_num > num:
-        print('Число слишком большое, попробуй еще раз')
-    elif user_num < num:
-        print('Число слишком маленькое, попробуй еще раз')
+    
+    if is_valid(user_num):
+        if user_num > num:
+            print('Число слишком большое, попробуй еще раз')
+            counter += 1
+        elif user_num < num:
+            print('Число слишком маленькое, попробуй еще раз')
+            counter += 1
+        else:
+            print('Число угадано за ', counter, ' попыток')
+            break
     else:
-        print('Число угадано')
-        break
+        print('ты че?')
